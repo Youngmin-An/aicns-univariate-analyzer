1253d774a768376777d26fce54b4af0d29d2ab8d
Youngmin-An <youngmin.develop@gmail.com>
2022-09-07 13:34:56 +0000

    [AICNS-30] Specify temp report spec

--- univariate/analyzer/analysis_report.py
+++ univariate/analyzer/analysis_report.py
@@ -9,4 +9,12 @@ class AnalysisReport:
     """
 
     def __init__(self):
-        pass
+        """
+            # todo: temp report, spec latter
+        """
+        self.period = None
+        self.period_error = None
+        self.regularity = None
+        self.distribution = None
+        self.plot = dict()
+  
