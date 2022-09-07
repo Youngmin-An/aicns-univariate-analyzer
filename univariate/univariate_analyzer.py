a7818acae5b058cc5f344176e212d5f43e2d8a92
Youngmin-An <youngmin.develop@gmail.com>
2022-09-07 10:55:36 +0000

    [AICNS-30] Use logging library

--- univariate/univariate_analyzer.py
+++ univariate/univariate_analyzer.py
@@ -6,6 +6,9 @@ from pyspark.sql import DataFrame
 from typing import Optional, List
 from univariate.hook import Hook
 from univariate.analyzer import AnalysisReport, Analyzer, RegularityAnalyzer
+import logging
+
+logger = logging.getLogger()
 
 
 class UnivariateAnalyzer:
@@ -27,36 +30,51 @@ class UnivariateAnalyzer:
         :param time_col: string column name containing pyspark.sql.DatetimeType
         :param hooks: list of hooks that want to be notified analysis reports
         """
+        logger.debug(f"Setup Analyzer start, ts: {ts}, val_col: {val_col}, time_col: {time_col}, hooks: {hooks or '[]'}")
         self.ts: DataFrame = ts
         self.hooks: List[Hook] = hooks or []  # todo : default post analysis hook
+        self.analysis_queue: List[Analyzer] = list()
         try:
             self.__validate_ts(val_col, time_col)
         except Exception as e:  # todo: Custom exception or specific
-            print(
+            logger.error(
                 "Error in validation ts, ts must be pyspark sql dataframe that has one DatatimeType column, "
                 "and has one timeseries value(float or number) column"
             )
             raise e
         else:
-            self.analysis_queue: List[Analyzer] = list()
             regularity_analyzer: Analyzer = RegularityAnalyzer()
             self.regularity_report: AnalysisReport = regularity_analyzer.analyze()
             self.__notify_report(self.regularity_report)
-            self.__enqueue_analysis_job(AnalysisReport["regular_type"])
+            self.__enqueue_analysis_job()
 
     def __validate_ts(self, val_col: str, time_col: str) -> bool:
         pass
 
     def __notify_report(self, report: AnalysisReport):
         map(lambda hook: hook.do_post_analysis(report), self.hooks)
+        logger.debug("Notify all hook")
 
     def __enqueue_analysis_job(self):
         """
 
-        :param regular_type:
         :return:
         """
-        if self.regularity_report["regular_type"] == "regular":
+        logger.debug("enqueue analysis job start")
+        if self.regularity_report.regularity == "regular":
             pass
-        elif self.regularity_report["regular_type"] == "irregular":
+        elif self.regularity_report.regularity == "irregular":
             pass
+
+    def analyze(self):
+        """
+
+        :return:
+        """
+        logger.debug("analyze called")
+        map(
+            lambda job: map(
+                lambda hook: hook.do_post_analysis(job.analyze), self.hooks
+            ),
+            self.analysis_queue,
+        )  # todo : Explore parallezing with spark sub(child) context?
