446489b8604255ecf8f859411c024f4cbe8f2a23
Youngmin-An <111176950+Youngmin-An@users.noreply.github.com>
2022-09-14 15:26:43 +0900

    Create Jenkinsfile.groovy

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,23 @@
+pipeline {
+    agent {
+        kubernetes {
+            containerTemplate{
+                name 'python-build'
+                image 'illacast/jenkins-agent:0.1.0'
+                command 'sleep'
+                args '99d'
+            }
+        }
+    }
+    stages {
+        stage('Build and deploy package') {
+            steps {
+                container('python-build'){
+                    sh "git config user.name 'semantic-release'"
+                    sh "git config user.email 'semantic-release@jenkins'"
+                    sh "semantic-release publish -D version_variable=setup.py:__version__ -D branch=develop -D commit_parser=semantic_release.history.scipy_parser"
+                }
+            }
+        }
+    }
+}
