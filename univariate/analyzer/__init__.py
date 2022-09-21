71aefee24233c1a334e05a5517f55186f595c29a
Youngmin-An <youngmin.develop@gmail.com>
2022-09-21 16:32:57 +0900

    FEAT: [AICNS-34] Add decomposition analyzer
    - In future, feat will extended multi period and multi strategies

--- univariate/analyzer/__init__.py
+++ univariate/analyzer/__init__.py
@@ -3,3 +3,4 @@ from univariate.analyzer.analyzer import Analyzer
 from univariate.analyzer.regularity_analyzer import RegularityAnalyzer
 from univariate.analyzer.descriptive_stat_analyzer import DescriptiveStatAnalyzer
 from univariate.analyzer.autocorrelation_analyzer import AutocorrelationAnalyzer
+from univariate.analyzer.decomposition_analyzer import DecompositionAnalyzer
